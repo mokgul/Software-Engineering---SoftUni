fb97477791711791c4646b2afd735aee9ab4fcaa
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-06-05 18:18:59 +0300

    Uploading lab 11 and 12 from data types

---
+++ Programming Fundamentals with C#/04.Data Types and Variables - Lab/11.Refactor Vol of Pyra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11.Refactor_Vol_of_Pyra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double heigth = double.Parse(Console.ReadLine());
+
+            double volume = (length * width * heigth) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/04.Data Types and Variables - Lab/12.Refactor Special Num/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12.Refactor_Special_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sum = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sum += digits % 10;
+                    digits /= 10;
+                }
+                bool special = false;
+                special = ((sum == 5) || (sum == 7) || (sum == 11));
+                Console.WriteLine("{0} -> {1}", i, special);
+            }
+
+        }
+    }
+}
