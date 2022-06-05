70b70578957b49a5fef0efee2f79422a4dcaf8c7
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-06-05 16:20:09 +0300

    Uploading 4,5 lab from data types

---
+++ Programming Fundamentals with C#/04.Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sumDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumDigits += digits % 10;
+                    digits /= 10;
+                }
+                bool special = (sumDigits == 5) ? true : (sumDigits == 7) ? true : (sumDigits == 11) ? true : false;
+                if (special)
+                    Console.WriteLine($"{i} -> True");
+                else
+                    Console.WriteLine($"{i} -> False");
+            }
+        }
+    }
+}
