250f29a505553dcc219847702fcd729e76d5a29b
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-06-08 17:50:34 +0300

    More ex 1, 2 data types

---
+++ Programming Fundamentals with C#/06.Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correctType = false;
+            int integer = 0;
+            float floatingPoint = 0f;
+            char characters = ' ';
+            bool boolean;
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (correctType = int.TryParse(input, out integer))
+                    Console.WriteLine($"{input} is integer type");
+
+                else if (correctType = float.TryParse(input, out floatingPoint))
+                    Console.WriteLine($"{input} is floating point type");
+
+                else if (correctType = char.TryParse(input, out characters))
+                    Console.WriteLine($"{input} is character type");
+
+                else if (correctType = bool.TryParse(input, out boolean))
+                    Console.WriteLine($"{input} is boolean type");
+
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/06.Data Types and Variables - More Exercise/02. From Left to The Right/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._From_Left_to_The_Right
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersAmount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbersAmount; i++)
+            {
+                string left = "";
+                string right = "";
+                bool whitespace = false;
+                string numberLine = Console.ReadLine();
+                for (int j = 0; j < numberLine.Length; j++)
+                {
+                    if (numberLine[j] == ' ') whitespace = true;
+
+                    if (!whitespace)
+                        left += numberLine[j];
+                    else
+                    {
+                        right += numberLine[j];
+                    }
+                }
+
+                long leftSum = 0;
+                long rightSum = 0;
+                long leftNum = long.Parse(left);
+                long rightNum = long.Parse(right);
+                long leftTemp = leftNum;
+                long rightTemp = rightNum;
+                while (leftNum != 0)
+                {
+                    leftSum += leftNum % 10;
+                    leftNum /= 10;
+                }
+
+                while (rightNum != 0)
+                {
+                    rightSum += rightNum % 10;
+                    rightNum /= 10;
+                }
+
+                if (leftTemp > rightTemp)
+                    Console.WriteLine(Math.Abs(leftSum));
+                else
+                {
+                    Console.WriteLine(Math.Abs(rightSum));
+                }
+            }
+        }
+    }
+}
