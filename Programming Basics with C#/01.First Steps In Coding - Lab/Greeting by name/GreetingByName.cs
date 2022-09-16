f85285253e6069b8d6b8f2a18b517e89b512c852
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-09-17 01:34:02 +0300

    asd

---
+++ Programming Basics with C#/01.First Steps In Coding - Lab/Greeting by name/GreetingByName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Greeting_by_name
+{
+    class GreetingByName
+    {
+        static void Main(string[] args)
+        {
+            string firstname = Console.ReadLine();
+            string lastname  = Console.ReadLine();
+            int age          = int.Parse(Console.ReadLine());
+            string town      = Console.ReadLine();
+            Console.WriteLine($"You are {firstname} {lastname}, a {age}-years old person from {town}.");
+            
+        }
+    }
+}
