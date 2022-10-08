004273fe0e4831b2b79a0ac9c6bb71f0c8bbeaa4
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-10-08 23:40:14 +0300

    Stacks and Queues - Exercises 1 - 3

---
+++ 03.Programming C# Advanced/02.Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        private static int[] arguments;
+        private static int[] elements;
+        static void Main(string[] args)
+        {
+            Stack<int> integers = new Stack<int>();
+            GetArguments();
+            PushToStack(arguments[0],integers,elements);
+            PopElements(arguments[1], integers);
+            if (integers.Count == 0) StackIsEmpty();
+            else IsElementInStack(arguments[2], integers);
+        }
+
+        static void StackIsEmpty() => Console.WriteLine("0");
+        static void IsElementInStack(int element, Stack<int> integers)
+            =>Console.WriteLine(integers.Contains(element) ? $"true" : $"{integers.Min()}");
+        static void PopElements(int count, Stack<int> integers)
+        {
+            for (int i = 0; i < count; i++)
+                integers.Pop();
+        }
+
+        static void GetArguments()
+        {
+            Program.arguments = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Program.elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        static void PushToStack(int count, Stack<int> integers,int[] elements)
+        {
+            for(int i = 0; i < count; i++)
+                integers.Push(elements[i]);
+        }
+    }
+}
---
+++ 03.Programming C# Advanced/02.Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        private static Stack<int> stack = new Stack<int>();
+
+        private static Predicate<Stack<int>> NotEmpty = c => c.Count > 0;
+        static void Main(string[] args)
+        {
+            Input(Program.stack);
+            PrintStack();
+        }
+
+        static void PrintStack()
+        {
+            if (Program.NotEmpty(Program.stack))
+                Console.WriteLine(string.Join(", ", Program.stack.ToList()));
+        }
+        
+        static void Input(Stack<int> stack)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmnds = Console.ReadLine().Split();
+                SwitchCommands(cmnds);
+            }
+        }
+        static void SwitchCommands(string[] cmnds)
+        {
+            switch (cmnds[0])
+            {
+                case "1": Push(int.Parse(cmnds[1])); break;
+                case "2": if (Program.NotEmpty(Program.stack)) PopElement(); break;
+                case "3": if (Program.NotEmpty(Program.stack)) PrintMax(); break;
+                case "4": if (Program.NotEmpty(Program.stack)) PrintMin(); break;
+            }
+        }
+        static void Push(int element) => Program.stack.Push(element);
+        static void PopElement() => Program.stack.Pop();
+        static void PrintMax() => Console.WriteLine($"{Program.stack.Max()}");
+        static void PrintMin() => Console.WriteLine($"{Program.stack.Min()}");
+
+    }
+}
