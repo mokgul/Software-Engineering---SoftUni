0f00950f5421bdabd6c471173e6a1470edfe6dab
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2022-10-11 21:11:28 +0300

    Stack and Queues Exercises 11,12

---
+++ 03.Programming C# Advanced/02.Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace _11._Key_Revolver
+{
+    internal class Mission
+    {
+        private static int _bulletPrice;
+        private static int _gunCapacity;
+        private static Stack<int> _bullets;
+        private static Queue<int> _locks;
+        private static int _money;
+        private static int _bulletsShot;
+        static void Main(string[] args)
+        {
+            Inputs();
+            Shooting();
+        }
+
+        private static void Shooting()
+        {
+            int bulletCount = 0;
+            while (true)
+            {
+                if (Mission._locks.Count == 0)
+                {
+                    Success();
+                    break;
+                } 
+                if(Mission._bullets.Count == 0)
+                {
+                    Failed();
+                    break;
+                }
+                int bullet = Mission._bullets.Peek();
+                int currentLock = Mission._locks.Peek();
+                if (bullet <= currentLock)
+                {
+                    Bang();
+                    bulletCount++;
+                }
+                else
+                {
+                    FailedToUnlock();
+                    bulletCount++;
+                }
+                if (bulletCount == Mission._gunCapacity && Mission._bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    bulletCount = 0;
+                }
+            }
+        }
+
+        private static void Failed()
+        {
+            Console.WriteLine($"Couldn't get through. Locks left: {Mission._locks.Count}");
+        }
+
+        private static void Success()
+        {
+            int earned = Mission._money - Mission._bulletsShot * Mission._bulletPrice;
+            Console.WriteLine($"{Mission._bullets.Count} bullets left. Earned ${earned}");
+        }
+
+        private static void FailedToUnlock()
+        {
+            Console.WriteLine("Ping!");
+            Mission._bullets.Pop();
+            Mission._bulletsShot++;
+        }
+
+        private static void Bang()
+        {
+            Console.WriteLine("Bang!");
+            Mission._bullets.Pop();
+            Mission._locks.Dequeue();
+            Mission._bulletsShot++;
+        }
+
+        private static void Inputs()
+        {
+            Mission._bulletPrice = int.Parse(Console.ReadLine());
+            Mission._gunCapacity = int.Parse(Console.ReadLine());
+            Mission._bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Mission._locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Mission._money = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ 03.Programming C# Advanced/02.Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class WaterGame
+    {
+        private static Queue<int> _cups;
+        private static Stack<int> _bottles;
+        private static int wasted;
+        static void Main(string[] args)
+        {
+            Inputs();
+            Filling();
+        }
+
+        private static void Filling()
+        {
+            while(true)
+            {
+                if (WaterGame._cups.Count == 0)
+                {
+                    Console.WriteLine($"Bottles: {string.Join(" ",WaterGame._bottles)}");
+                    Console.WriteLine($"Wasted litters of water: {WaterGame.wasted}");
+                    break;
+                }
+                int cup = WaterGame._cups.Peek();
+                while (cup > 0)
+                {
+                    if (WaterGame._bottles.Count == 0)
+                    {
+                        Console.WriteLine($"Cups: {string.Join(" ",WaterGame._cups)}");
+                        Console.WriteLine($"Wasted litters of water: {WaterGame.wasted}");
+                        return;
+                    }
+                    int bottle = WaterGame._bottles.Peek();
+                    if (bottle <= cup)
+                    {
+                        WaterGame._bottles.Pop();
+                        if (bottle == cup)
+                            WaterGame._cups.Dequeue();
+                        cup -= bottle;
+                    }
+                    else
+                    {
+                        WaterGame._bottles.Pop();
+                        WaterGame._cups.Dequeue();
+                        WaterGame.wasted += bottle - cup;
+                        cup -= bottle;
+                    }
+                }
+            }
+        }
+
+        private static void Inputs()
+        {
+            WaterGame._cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            WaterGame._bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+        }
+    }
+}
