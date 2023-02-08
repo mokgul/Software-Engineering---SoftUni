c27e9597fc9247c28eeac2a228ae96750e68a29c
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2023-02-08 14:25:27 +0200

    fix, wrong code on a task

--- 04.Programming C# OOP/14.Design Patterns - Lab/Facade/Program.cs
+++ 04.Programming C# OOP/14.Design Patterns - Lab/Facade/Program.cs
@@ -1,45 +1,22 @@
 ﻿using System;
-using System.Threading.Channels;
 
-namespace _06._Balanced_Brackets
+namespace Facade
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int interval = int.Parse(Console.ReadLine());
-            int counterOpen = 0;
-            int counterClose = 0;
-            for (int i = 1 ; i <= interval; i++)
-            {
-                string massage = Console.ReadLine();
- 
-                if (massage=="(")
-                {
-                    counterOpen++;
-                }
-                else if (massage==")")
-                {
-                    counterClose++;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            if (counterClose == 0 && counterOpen == 0)
-            {
-                Console.WriteLine("UNBALANCED");
-            }
-            else if(counterClose==counterOpen)
-            {
-                Console.WriteLine("BALANCED");
-            }
-            else
-            {
-                Console.WriteLine("UNBALANCED");
-            }
+            var car = new CarBuilderFacade()
+                .Info
+                    .WithType("BMW")
+                    .WithColor("Black")
+                .WithNumberOfDoors(5)
+                .Built
+                    .InCity("Leipzig")
+                    .AtAddress("Some address 254")
+                .Build();
 
+            Console.WriteLine(car);
         }
     }
 }
