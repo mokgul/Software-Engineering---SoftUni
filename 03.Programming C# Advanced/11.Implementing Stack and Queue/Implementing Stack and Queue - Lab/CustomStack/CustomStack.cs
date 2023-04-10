ed0398aafb1bc2ed63d8f3c0415882012d9dfbda
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2023-04-10 23:08:41 +0300

    CustomStack

---
+++ 03.Programming C# Advanced/11.Implementing Stack and Queue/Implementing Stack and Queue - Lab/CustomStack/CustomStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CustomStack;
+
+public class CustomStack
+{
+    //Private fields
+    private const int Capacity = 4;
+    private int[] items;
+    private int count;
+
+    //Constructor
+    public CustomStack()
+    {
+        count = 0;
+        items = new int[Capacity];
+    }
+
+    //Public properties
+    public int Count => count;
+
+    //Public methods
+    public void Push(int item)
+    {
+        if(items.Length == count)
+            Resize();
+
+        items[count] = item;
+        count++;
+    }
+
+    public int Pop()
+    {
+        if (items.Length == 0)
+            throw new InvalidOperationException("CustomStack is empty");
+
+        var removed = items[count - 1];
+        int[] copy = new int[items.Length - 1];
+
+        for(int i = 0; i < count; i++)
+            copy[i] = items[i];
+
+        items = copy;
+        count--;
+        return removed;
+    }
+
+    public int Peek()
+    {
+        if (items.Length == 0)
+            throw new InvalidOperationException("CustomStack is empty");
+
+        int item = items[count - 1];
+        return item;
+    }
+
+    public void ForEach(Action<int> action)
+    {
+        for(int i = 0; i < count; i++)
+            action(items[i]);
+    }
+
+    //Private methods
+    private void Resize()
+    {
+        int[] copy = new int[items.Length * 2];
+        for (int i = 0; i < items.Length; i++)
+            copy[i] = items[i];
+        items = copy;
+    }
+}
---
+++ 03.Programming C# Advanced/11.Implementing Stack and Queue/Implementing Stack and Queue - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomStack
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack  = new CustomStack();
+            for (int i = 0; i < 100; i++)
+            {
+                stack.Push(new Random().Next(0, 150));
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            stack.ForEach(i => Console.Write(i.ToString() + ' '));
+        }
+    }
+}
