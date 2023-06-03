a0dea05fe3607168c1ad44140899254a6316b013
Dian Radoslavov <106612784+mokgul@users.noreply.github.com>
2023-06-03 23:30:18 +0300

    ASP-NET-Core Introduction - MVC Intro Demo

---
+++ 07.Programming ASP.NET Fundamentals/02.ASP.NET Core Introduction - Exercise/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+using System.Text.Json;
+using Microsoft.Net.Http.Headers;
+using static MVCIntroDemo.Seeding.ProductsData;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword = "")
+        {
+            if (String.IsNullOrEmpty(keyword))
+                return View(_products);
+
+            IEnumerable<ProductViewModel> filteredProducts =
+                _products.Where(p => p.Name.ToLower().Contains(keyword.ToLower())).ToArray();
+            return View(filteredProducts);
+        }
+
+        public IActionResult ById(int id)
+        {
+            ProductViewModel? product = _products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return this.RedirectToAction("My-Products");
+            }
+            return View(product);
+
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            return Json(_products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            var text = string.Empty;
+            foreach (var item in _products)
+            {
+                text += $"Product {item.Id}: {item.Name} - {item.Price} lv.";
+                text += "\r\n";
+            }
+
+            return Content(text);
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in _products)
+                sb.AppendLine($"Product: {item.Id}: {item.Name} - {item.Price:f2} lv.");
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
---
+++ 07.Programming ASP.NET Fundamentals/02.ASP.NET Core Introduction - Exercise/MVCIntroDemo/Seeding/ProductsData.cs
@@ -0,0 +1,28 @@
+﻿namespace MVCIntroDemo.Seeding;
+
+using Models.Product;
+
+public static class ProductsData
+{
+    public static IEnumerable<ProductViewModel> _products = new List<ProductViewModel>()
+    {
+        new ProductViewModel()
+        {
+            Id = 1,
+            Name = "Cheese",
+            Price = 7.00
+        },
+        new ProductViewModel()
+        {
+            Id = 2,
+            Name = "Ham",
+            Price = 5.50
+        },
+        new ProductViewModel()
+        {
+            Id = 3,
+            Name = "Bread",
+            Price = 1.50
+        },
+    };
+}
