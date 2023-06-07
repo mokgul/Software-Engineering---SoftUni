4949cbde1090183c922f40979eb63e934c33560a
Dian Radoslavov <106612784+mokgul@users.noreply.github.com>
2023-06-07 18:58:38 +0300

    ChatApp and TextSplitter

---
+++ 07.Programming ASP.NET Fundamentals/02.ASP.NET Core Introduction - Exercise/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> s_messages = new List<KeyValuePair<string, string>>();
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if (s_messages.Count() < 1)
+                return View(new ChatViewModel());
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = s_messages
+                    .Select(m => new MessageViewModel()
+                    {
+                        Sender = m.Key,
+                        MessageText = m.Value
+                    })
+                    .ToList()
+            };
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+
+            s_messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ 07.Programming ASP.NET Fundamentals/02.ASP.NET Core Introduction - Exercise/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitter.Models;
+
+namespace TextSplitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitTextArray = model
+                .Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splitTextArray);
+            return RedirectToAction("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
